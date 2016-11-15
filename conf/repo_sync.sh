bdf5feb8c424d635d7a53b166b8e544ce8af23c1
Yongbok Kim <ybkim@rockplace.co.kr>
2016-11-16 00:19:44 +0900

    Update conf/repo_sync.sh

--- conf/repo_sync.sh
+++ conf/repo_sync.sh
@@ -13,13 +13,13 @@ function f_reposync {
       if [[ -d "$REPO_DIR/$repos" ]]; then
           echo "Repo sync..."
           reposync --downloadcomps --download-metadata --newest-only -r $repos $REPO_DIR
-          cd $REPO_DIR/$repos && createrepo .
+          cd $REPO_DIR/$repos && createrepo . && cd ..
           echo "Done"
 
       else
           echo "Repo sync..."
           reposync --downloadcomps --download-metadata -r $repos $REPO_DIR
-          cd $REPO_DIR/$repos && createrepo .
+          cd $REPO_DIR/$repos && createrepo . && cd ..
           echo "Done"
       fi
   done
