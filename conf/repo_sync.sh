abaaf59746bca48c7685c6759a34e871c8cb5c03
Yongbok Kim <ybkim@rockplace.co.kr>
2016-11-15 23:30:52 +0900

    Dockerfile - RepoSync RHEL

---
+++ conf/repo_sync.sh
@@ -0,0 +1,56 @@
+#################################################
+# Title      : RepoSync RHEL                    #
+# Date       : 2016.11.03                       #
+# Maintainer : Yongbok Kim (ruo91@yongbok.net)  #
+#################################################
+#!/bin/bash
+REPO_DIR="/opt"
+REPO_VER="rhel-$(cat /etc/redhat-release | awk '{print $7}' | cut -d '.' -f '1')"
+REPO_LIST="$REPO_VER-server-rpms $REPO_VER-server-extras-rpms $REPO_VER-server-optional-rpms $REPO_VER-server-rh-common-rpms"
+
+function f_reposync {
+  if [[ -d "$REPO_DIR/$REPO_LIST" ]]; then
+      for repos in ${REPO_LIST}; do
+          echo "Repo sync..."
+          reposync --downloadcomps --download-metadata --newest-only -r $repos $REPO_DIR
+          cd $REPO_DIR/$repos && createrepo .
+          echo "Done"
+      done
+
+  else
+      for repos in ${REPO_LIST}; do
+          echo "Repo sync..."
+          reposync --downloadcomps --download-metadata -r $repos $REPO_DIR
+          cd $REPO_DIR/$repos && createrepo .
+          echo "Done"
+      done
+  fi
+}
+
+function f_help {
+  echo "Usage: $ARG_0 [Options]"
+  echo
+  echo "- Options"
+  echo "y, yes"
+  echo "n, no"
+  echo
+}
+
+# Main
+ARG_0="$0"
+ARG_1="$1"
+
+case ${ARG_1} in
+    y|Y|yes|YES)
+        f_reposync
+    ;;
+
+    n|N|no|NO)
+        exit 0
+    ;;
+
+    *)
+        f_help
+    ;;
+esac
+
---
+++ rhel6.x
@@ -0,0 +1,41 @@
+#
+# Dockerfile - RepoSync RHEL
+#
+# - Build
+# docker build --rm -t rhel6:reposync -f rhel6.x .
+#
+# - Run
+# docker run -d --name="rhel6-reposync" -h "rhel6-reposync" -v /path/to/host/directory:/path/to/container/directory rhel6:reposync
+#
+# Use the base images
+FROM ruo91/rhel:68
+MAINTAINER Yongbok Kim <ruo91@yongbok.net>
+
+# Subscription
+ENV RHNID "Change_Your_RHNID"
+ENV RHNPW "Change_Your_RHNPW"
+RUN subscription-manager register \
+ --auto-attach --username="$RHNID" --password="$RHNPW" --name="docker_reposync" \
+ && subscription-manager repos --disable='*' \
+ && subscription-manager repos \
+ --enable=rhel-6-server-rpms \
+ --enable=rhel-6-server-extras-rpms \
+ --enable=rhel-6-server-optional-rpms \
+ --enable=rhel-6-server-rh-common-rpms \
+ && yum clean all && yum repolist
+ 
+# The install package for reposync
+RUN rpm --rebuilddb && yum install -y yum-utils createrepo
+
+# Variable
+ENV SRC_DIR /opt
+WORKDIR $SRC_DIR
+
+# Add scripts
+ADD conf/repo_sync.sh /bin/repo_sync.sh
+RUN sed -i '/^repo_dir=/ s:.*:repo_dir=\"$SRC_DIR\":' /bin/repo_sync.sh
+RUN chmod a+x /bin/repo_sync.sh
+
+# repo sync
+CMD ["/bin/bash", "/bin/repo_sync.sh", "y"]
+
